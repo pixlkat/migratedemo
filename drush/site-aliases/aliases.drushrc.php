ef25b71b52464309fad1386f67af73969412e2a4
Stephanie Bridges <steph.bridges@MacBook-Pro.local>
2017-08-21 18:43:33 -0400

    MD-0: Updates from updating BLT/lightning.

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -359,14 +359,18 @@
 #    'uri' => 'http://example.com',
 #    'root' => $aliases['dev']['root'],
 #  ) + $aliases['server'];
-// [vagrant_machine_name].local
+
+// Local environment.
 $aliases['migratedemo.local'] = array(
-  // /var/www/[vagrant_machine_name]/docroot
   'root' => '/var/www/migratedemo/docroot',
-  // vagrant_hostname
   'uri' => 'http://local.migratedemo.com',
-  // vagrant_hostname
-  'remote-host' => 'local.migratedemo.com',
-  'remote-user' => 'vagrant',
-  'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
-);
+  );
+// Add remote connection options when alias is used outside VM.
+if ('vagrant' != $_SERVER['USER']) {
+  $aliases['migratedemo.local'] += array(
+    'remote-host' => 'local.migratedemo.com',
+    'remote-user' => 'vagrant',
+    'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+  );
+}
+
